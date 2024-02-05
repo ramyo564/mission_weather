0994176bca906e2831dfcac6c4f85febfeb8c31a
ramyo564 <yohan032yohan@gmail.com>
2024-02-05 18:40:59 +0900

    GET / read / diary - 컨트롤러 작성

--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -1,13 +1,12 @@
 package zerobase.weather.controller;
 
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import zerobase.weather.domain.Diary;
 import zerobase.weather.service.DiaryService;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class DiaryController {
@@ -24,6 +23,14 @@ public class DiaryController {
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
             LocalDate date, @RequestBody String text){
         diaryService.createDiary(date, text);
+    }
+
+    @GetMapping("/read/diary")
+    List<Diary> readDiary(
+            @RequestParam(value = "date")
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate date) {
+        return diaryService.readDiary(date);
     }
 
 }
