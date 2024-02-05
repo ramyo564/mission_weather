f1861df939874e94bb57a92125ff3a7fc61332a6
ramyo564 <yohan032yohan@gmail.com>
2024-02-05 17:27:37 +0900

    레퍼지토리 패키지로 이동 및 @Repository 추가

--- src/main/java/zerobase/weather/repository/JpaMemoRepository.java
+++ src/main/java/zerobase/weather/repository/JpaMemoRepository.java
@@ -1,7 +1,8 @@
-package zerobase.weather;
+package zerobase.weather.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import zerobase.weather.domain.Memo;
-
+@Repository
 public interface JpaMemoRepository extends JpaRepository<Memo, Integer> {
 }
