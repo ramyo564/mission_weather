cb8e2611cc58bb40fed65bf391d54f1ca0e54a1b
ramyo564 <yohan032yohan@gmail.com>
2024-02-05 18:23:11 +0900

    매개변수 수정

--- src/main/java/zerobase/weather/repository/DiaryRepository.java
+++ src/main/java/zerobase/weather/repository/DiaryRepository.java
@@ -2,9 +2,9 @@ package zerobase.weather.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import zerobase.weather.domain.Memo;
+import zerobase.weather.domain.Diary;
 
 @Repository
-public interface DiaryRepository extends JpaRepository<Memo, Integer> {
+public interface DiaryRepository extends JpaRepository<Diary, Integer> {
 
 }
